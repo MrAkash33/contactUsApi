1b35c72f6cba46d54c18a4d18b048a773765d67c
Mr Akash <79826594+MrAkash33@users.noreply.github.com>
2022-10-11 12:20:21 +0530

    Add files via upload

--- controllers/feedback.js
+++ controllers/feedback.js
@@ -1,4 +1,5 @@
 const { customerModal } = require("../modal/contact")
+var nodemailer = require('nodemailer');
 
 const feedBack = async (req,res,next) => {
     const data = req.body
@@ -8,8 +9,42 @@ const feedBack = async (req,res,next) => {
         message :req.body.message
     })
     const savedData = await user.save()
-    res.json(savedData)
-    console.log(data,savedData,"data")
+      var transporter = nodemailer.createTransport({
+        host: 'smtp.gmail.com',   
+        port: 587,
+        secure:false,
+        auth: {
+          user: 'artstartdotin@gmail.com',
+          pass: process.env.PASSWORD
+        }
+      });
+      var mailOptions = {
+        from: 'artstartdotin@gmail.com',
+        to: req.body.email,
+        subject: 'Thankyou for contacting!',
+        text: ` Hii ${req.body.username} thanks for contacting. I will touch you soon!`
+      };
+      var mailOptionsSender = {
+        from: 'artstartdotin@gmail.com',
+        to: 'artstartdotin@gmail.com',
+        subject: 'Someone Wants to connect With you !',
+        text: ` Hello Artstart ${req.body.username} wants to connect with you please contact him to further query!`
+      };
+      transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+          console.log(error);
+        } else {
+          console.log('Email sent: ' + info.response);
+            transporter.sendMail(mailOptionsSender, function(error, info){
+                if (error) {
+                console.log(error);
+                } else {
+                console.log('Email sent: ' + info.response);
+                }
+            });
+        }
+      });
+      res.json(savedData)
 }
 
 module.exports = {
